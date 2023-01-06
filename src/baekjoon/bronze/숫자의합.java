5d613298cd70c38afa57999f8442b5838ebbcee0
HOng <moonshin123@naver.com>
2023-01-07 03:19:25 +0900

    숫자의합

---
+++ src/baekjoon/bronze/숫자의합.java
@@ -0,0 +1,17 @@
+package baekjoon.bronze;
+
+import java.util.Scanner;
+
+public class 숫자의합 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+        String str = scan.next();
+        char[] chArr = str.toCharArray();
+        int answer = 0;
+        for(int i = 0; i < chArr.length; i++) {
+            answer += chArr[i] - '0';
+        }
+        System.out.println(answer);
+    }
+}
